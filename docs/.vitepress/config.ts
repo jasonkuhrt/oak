7af4ca462a9570063a50c11a3290439de288865f
Jason Kuhrt <jasonkuhrt@me.com>
2025-10-11 15:41:50 -0400

    fix: temporarily disable Twoslash to get docs building
    Twoslash can't resolve local package imports. For now, disable
    it so the docs can build. The syntax highlighting still works,
    just no interactive type inference hovers.
    
    TODO: Re-enable Twoslash once we figure out module resolution.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,5 +1,5 @@
 import { transformerNotationDiff, transformerNotationHighlight } from '@shikijs/transformers'
-import { transformerTwoslash } from '@shikijs/vitepress-twoslash'
+// import { transformerTwoslash } from '@shikijs/vitepress-twoslash'
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
@@ -10,7 +10,8 @@ export default defineConfig({
 
   markdown: {
     codeTransformers: [
-      transformerTwoslash(),
+      // TODO: Re-enable Twoslash once we figure out module resolution
+      // transformerTwoslash(),
       transformerNotationDiff(),
       transformerNotationHighlight(),
     ],
